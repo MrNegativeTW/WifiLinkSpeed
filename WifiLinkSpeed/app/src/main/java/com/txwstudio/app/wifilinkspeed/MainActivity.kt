914693111c25d7ffed6a4f6b1dbeac35de6c77a6
TrevorWu <wowpapa3232@gmail.com>
2020-02-21 10:19:27 +0800

    Adjust layout
    - Add font family
    - Adjust layout

--- WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/MainActivity.kt
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/MainActivity.kt
@@ -5,55 +5,69 @@ import android.content.Intent
 import android.net.Uri
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var ssid: TextView? = null
-    private var rssi: TextView? = null
-    private var linkSpeed: TextView? = null
+    private val toolBar: Toolbar ?= null
     private var mHandler: Handler? = null
     private val updateInterval = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupTheme()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById()
+        setSupportActionBar()
 
         getWifiInfo()
         mHandler = Handler()
+
     }
 
     override fun onResume() {
         super.onResume()
         startRepeatingTask()
     }
-
     override fun onPause() {
         super.onPause()
         stopRepeatingTask()
     }
 
 
-    private fun findViewById() {
-        ssid = findViewById(R.id.textview_ssid_value)
-        rssi = findViewById(R.id.textview_status_value)
-        linkSpeed = findViewById(R.id.textview_linkspeed_value)
+    private fun setupTheme() {
+        setTheme(R.style.AppTheme_NoActionBar)
+
+        // Fix white status bar when sdk <= 21
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            val window = this.window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.darkMode_colorPrimary)
+        }
     }
+
+    private fun setSupportActionBar() {
+        toolBar?.findViewById<Toolbar>(R.id.toolbar_main)
+        setSupportActionBar(toolBar)
+    }
+
 
     private fun getWifiInfo() {
         val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val info: WifiInfo = wifiManager.connectionInfo
 
-        ssid?.text = info.ssid
-        rssi?.text = info.rssi.toString()
-        linkSpeed?.text = info.linkSpeed.toString()
+        textview_ssid_value.text = info.ssid.substring(1, info.ssid.length - 1)
+        textview_rssi_value.text = info.rssi.toString()
+        textview_linkspeed_value.text = info.linkSpeed.toString()
     }
-
 
     var mStatusChecker: Runnable = object : Runnable {
         override fun run() {
@@ -65,16 +79,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun startRepeatingTask() {
+    private fun startRepeatingTask() {
         mStatusChecker.run()
     }
 
-    fun stopRepeatingTask() {
+    private fun stopRepeatingTask() {
         mHandler?.removeCallbacks(mStatusChecker)
     }
 
 
-    fun openSpeedtest(v: View) {
+    fun openSpeedTest(view: View) {
         var intent = packageManager.getLaunchIntentForPackage("org.zwanoo.android.speedtest")
         if (intent != null) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -86,8 +100,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun openFastCom(v: View) {
+    fun openFastCom(view: View) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://fast.com"))
         startActivity(intent)
+    }
+
+    fun openWifiSettings(view: View) {
+        startActivity(Intent(android.provider.Settings.ACTION_WIFI_SETTINGS))
     }
 }
