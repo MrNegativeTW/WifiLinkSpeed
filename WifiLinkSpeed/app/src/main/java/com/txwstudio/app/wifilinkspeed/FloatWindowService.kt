42378b6d24596aad9ad6b013c5f96cc162661d44
TrevorWu <wowpapa3232@gmail.com>
2020-02-24 13:56:58 +0800

    Float Window Move Action

--- WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/FloatWindowService.kt
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/FloatWindowService.kt
@@ -1,5 +1,6 @@
 package com.txwstudio.app.wifilinkspeed
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -7,17 +8,16 @@ import android.graphics.Color
 import android.graphics.PixelFormat
 import android.os.Build
 import android.os.IBinder
-import android.view.Gravity
-import android.view.WindowManager
+import android.view.*
 import android.widget.LinearLayout
 import android.widget.Toast
 
 
-class FloatWindowService: Service() {
+class FloatWindowService : Service() {
 
     private var windowManager: WindowManager? = null
     private var params: WindowManager.LayoutParams? = null
-    private var linearLayout: LinearLayout? = null
+    private var floatLayout: View? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -25,45 +25,81 @@ class FloatWindowService: Service() {
 
     override fun onCreate() {
         super.onCreate()
-
         Toast.makeText(this, "Service onCreate", Toast.LENGTH_SHORT).show()
 
-        // Initial
-        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        linearLayout = LinearLayout(this)
+        initWindowManager()
+        initLayout()
+        layoutOnTouch()
 
-        // Create Layout
-        val linearLayoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT)
-        linearLayout?.setBackgroundColor(Color.argb(125, 200, 200, 200))
-        linearLayout?.layoutParams = linearLayoutParams
-
-
-        /**
-         * Setup WindowManager
-         * TYPE_SYSTEM_OVERLAY deprecated in API level 26 (Oreo).
-         * */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            params = WindowManager.LayoutParams(50, 50,
-                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                    PixelFormat.TRANSLUCENT)
-        } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1){
-            params = WindowManager.LayoutParams(
-                    WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                    PixelFormat.TRANSLUCENT)
-        }
-        params?.gravity = Gravity.END or Gravity.BOTTOM
-        params?.title = "Title Here"
-
-
-        windowManager?.addView(linearLayout, params)
+        windowManager?.addView(floatLayout, params)
     }
+
+
+    /**
+     * Setup WindowManager
+     * TYPE_SYSTEM_OVERLAY deprecated in API level 26 (Oreo).
+     * */
+    private fun initWindowManager() {
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        params = WindowManager.LayoutParams()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params?.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1) {
+            params?.type = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY
+        }
+
+        params?.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        params?.format = PixelFormat.TRANSLUCENT
+
+        params?.width = WindowManager.LayoutParams.WRAP_CONTENT
+        params?.height = WindowManager.LayoutParams.WRAP_CONTENT
+        params?.gravity = Gravity.CENTER
+    }
+
+    /**
+     * Setup layout, get from service_float_window.xml
+     * */
+    private fun initLayout() {
+        floatLayout = LayoutInflater.from(application).inflate(R.layout.service_float_window, null)
+    }
+
+
+    //TODO(performOnClick)
+    @SuppressLint("ClickableViewAccessibility")
+    private fun layoutOnTouch() {
+        var touchedX: Float? = null
+        var touchedY: Float? = null
+        var originalX: Int? = null
+        var originalY: Int? = null
+
+        floatLayout?.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        originalX = params?.x
+                        originalY = params?.y
+
+                        touchedX = event.rawX
+                        touchedY = event.rawY
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        params?.x = (originalX!! + event.rawX - touchedX!!).toInt()
+                        params?.y = (originalY!! + event.rawY - touchedY!!).toInt()
+
+                        windowManager?.updateViewLayout(floatLayout, params)
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
-        windowManager?.removeViewImmediate(linearLayout)
+        windowManager?.removeView(floatLayout)
         Toast.makeText(this, "Service onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
