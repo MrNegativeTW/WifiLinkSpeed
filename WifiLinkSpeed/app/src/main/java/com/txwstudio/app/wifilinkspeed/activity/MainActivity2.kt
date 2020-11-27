5e5a051e0e570515fc376855c28c30fb5aa7298d
TrevorWu <wowpapa3232@gmail.com>
2020-11-27 17:25:29 +0800

    new activity with fragment

---
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/activity/MainActivity2.kt
@@ -0,0 +1,16 @@
+package com.txwstudio.app.wifilinkspeed.activity
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.txwstudio.app.wifilinkspeed.R
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
