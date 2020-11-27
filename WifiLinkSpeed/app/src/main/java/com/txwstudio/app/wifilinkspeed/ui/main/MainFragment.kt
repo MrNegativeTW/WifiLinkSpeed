351b8e6c4adefe7c00150085a4c56afe07a0b83a
TrevorWu <wowpapa3232@gmail.com>
2020-11-27 19:07:51 +0800

    Get MainFragment to work

--- WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/ui/main/MainFragment.kt
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/ui/main/MainFragment.kt
@@ -1,11 +1,17 @@
 package com.txwstudio.app.wifilinkspeed.ui.main
 
+import android.content.Context
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.txwstudio.app.wifilinkspeed.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.txwstudio.app.wifilinkspeed.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,16 +22,58 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+        private const val updateInterval = 1
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val liveCamViewModel: MainFragmentViewModel by viewModels()
+    private lateinit var binding: FragmentMainBinding
+
+    private lateinit var mHandler: Handler
+    private var updateWifiInfoTask = object : Runnable {
+        override fun run() {
+            try {
+                getWifiInfo()
+            } finally {
+                mHandler.postDelayed(this, updateInterval.toLong())
+            }
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeUi()
+        mHandler = Handler(Looper.getMainLooper())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getWifiInfo()
+        mHandler.post(updateWifiInfoTask)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mHandler.removeCallbacks(updateWifiInfoTask)
+    }
+
+    private fun subscribeUi() {
+
+    }
+
+    private fun getWifiInfo() {
+        val wifiManager = context?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val info: WifiInfo = wifiManager.connectionInfo
+
+        binding.textViewMainFragmentCurrentSsidContent.text = info.ssid.substring(1, info.ssid.length - 1)
+        binding.textViewMainFragmentCurrentWifiMacContent.text = info.bssid
+        binding.textViewMainFragmentCurrentRssiContent.text = info.rssi.toString() + " dBm"
+        binding.textViewMainFragmentCurrentLinkSpeedContent.text = info.linkSpeed.toString() + " Mbps"
+    }
 
 }
--- WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/ui/main/MainFragmentViewModel.kt
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/ui/main/MainFragmentViewModel.kt
@@ -1,7 +1,23 @@
 package com.txwstudio.app.wifilinkspeed.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
-class MainFragmentViewModel : ViewModel() {
-    // TODO(View Model)
+class MainFragmentViewModel(application: Application) : AndroidViewModel(application) {
+
+    val currentSsid = MutableLiveData<String>()
+    val currentBssid = MutableLiveData<String>()
+    val currentRssi = MutableLiveData<String>()
+    val currentLinkSpeed = MutableLiveData<String>()
+
+    fun getWifiInfo() {
+//        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+//        val info: WifiInfo = wifiManager.connectionInfo
+
+//        currentSsid.value = info.ssid.substring(1, info.ssid.length - 1)
+//        currentBssid.value = info.bssid
+//        currentRssi.value = info.rssi.toString() + " dBm"
+//        currentLinkSpeed.value = info.linkSpeed.toString() + " Mbps"
+    }
 }
