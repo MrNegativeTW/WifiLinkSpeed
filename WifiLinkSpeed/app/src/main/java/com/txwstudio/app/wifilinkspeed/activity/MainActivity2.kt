ea1bcfb3c7417b7e204bbc56134c2f15bbd06e31
TrevorWu <wowpapa3232@gmail.com>
2020-11-27 18:09:56 +0800

    MainActivity2 toolbar

--- WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/activity/MainActivity2.kt
+++ WifiLinkSpeed/app/src/main/java/com/txwstudio/app/wifilinkspeed/activity/MainActivity2.kt
@@ -5,12 +5,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.txwstudio.app.wifilinkspeed.R
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setupToolBar()
+    }
+
+    private fun setupToolBar() {
+        setSupportActionBar(toolbar_mainFrag)
     }
 }
